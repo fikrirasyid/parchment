7630c5485a447233af427ad4eee2b787e2a06937
Fikri Rasyid <fikrirasyid@gmail.com>
2015-02-11 08:23:38 +0700

    Init from underscores.me

---
+++ inc/jetpack.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Jetpack Compatibility File
+ * See: http://jetpack.me/
+ *
+ * @package Manuscript
+ */
+
+/**
+ * Add theme support for Infinite Scroll.
+ * See: http://jetpack.me/support/infinite-scroll/
+ */
+function manuscript_jetpack_setup() {
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'footer'    => 'page',
+	) );
+}
+add_action( 'after_setup_theme', 'manuscript_jetpack_setup' );
