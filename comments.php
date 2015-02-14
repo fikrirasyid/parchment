f46b6b8bdfdc53546389af0ed66d25c0942befdc
Fikri Rasyid <fikrirasyid@gmail.com>
2015-02-14 21:59:31 +0700

    Adding basic styling for comment list

--- comments.php
+++ comments.php
@@ -43,6 +43,7 @@ if ( post_password_required() ) {
 				wp_list_comments( array(
 					'style'      => 'ol',
 					'short_ping' => true,
+					'avatar_size'=> 60
 				) );
 			?>
 		</ol><!-- .comment-list -->
