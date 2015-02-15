ae97a70f69adca99b88aabf45c5716cc6d50e0c9
Fikri Rasyid <fikrirasyid@gmail.com>
2015-02-15 18:42:23 +0700

    Removing sidebar

--- footer.php
+++ footer.php
@@ -10,6 +10,29 @@
 
 	</div><!-- #content -->
 
+	<div id="drawer">
+		<div class="drawer-content sliding-content" data-direction="left">
+			<div class="drawer-header">
+				<a href="#" data-target-id="drawer" class="genericon genericon-close-alt toggle-button">
+					<span class="label">Close Drawer</span>
+				</a>
+				<h2 class="site-name"><?php bloginfo('name' ); ?></h2>
+			</div><!-- .drawer-header -->
+
+			<div class="drawer-navigation">
+				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			</div><!-- .drawer-navigation -->
+			
+			<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>	
+			<div class="drawer-widgets">
+				<?php dynamic_sidebar( 'sidebar-1' ); ?>				
+			</div><!-- .drawer-widgets -->
+			<?php endif; ?>
+		</div><!-- .drawer-content -->
+
+		<div class="drawer-overlay toggle-button" data-target-id="drawer"></div>
+	</div><!-- #drawer -->	
+
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'manuscript' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'manuscript' ), 'WordPress' ); ?></a>
--- header.php
+++ header.php
@@ -21,6 +21,8 @@
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'manuscript' ); ?></a>
 
+	<a href="#drawer" data-target-id="drawer" id="toggle-drawer" class="genericon genericon-menu toggle-button"><span class="label"><?php _e( 'Menu', 'manuscript' ); ?></span></a>
+
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
