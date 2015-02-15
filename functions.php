51774bed58cc40faf84076b0df19bf0a033e396d
Fikri Rasyid <fikrirasyid@gmail.com>
2015-02-15 16:31:33 +0700

    Improving civil footnotes integration UX

--- functions.php
+++ functions.php
@@ -108,6 +108,8 @@ function manuscript_scripts() {
 
 	wp_enqueue_script( 'manuscript-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+	wp_enqueue_script( 'manuscript-script', get_template_directory_uri() . '/js/manuscript.js', array( 'jquery' ), '20150215', true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
