2a998a38d637ae87b73db8f87361b402e139db4c
Fikri Rasyid <fikrirasyid@gmail.com>
2015-02-16 07:27:02 +0700

    Prevent printing empty posted_on

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -70,9 +70,14 @@ if ( ! function_exists( 'manuscript_posted_on' ) ) :
  * Prints HTML with meta information for the current post-date/time and author.
  */
 function manuscript_posted_on() {
+	$author = get_the_author();
+
+	if( ! $author )
+		return;
+
 	$byline = sprintf(
 		_x( 'by %s', 'post author', 'manuscript' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( $author ) . '</a></span>'
 	);
 
 	echo '<span class="byline"> ' . $byline . '</span>';
