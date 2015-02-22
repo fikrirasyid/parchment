860cbe40f361e1adee3bef508636b922753edac1
Fikri Rasyid <fikrirasyid@gmail.com>
2015-02-22 07:13:01 +0700

    Removing body classes filter

--- inc/extras.php
+++ inc/extras.php
@@ -7,22 +7,6 @@
  * @package Manuscript
  */
 
-/**
- * Adds custom classes to the array of body classes.
- *
- * @param array $classes Classes for the body element.
- * @return array
- */
-function manuscript_body_classes( $classes ) {
-	// Adds a class of group-blog to blogs with more than 1 published author.
-	if ( is_multi_author() ) {
-		$classes[] = 'group-blog';
-	}
-
-	return $classes;
-}
-add_filter( 'body_class', 'manuscript_body_classes' );
-
 if ( version_compare( $GLOBALS['wp_version'], '4.1', '<' ) ) :
 	/**
 	 * Filters wp_title to print a neat <title> tag based on what is being viewed.
