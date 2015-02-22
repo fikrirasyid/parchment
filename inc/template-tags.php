2251e215cf58aef43ea971cb03151ec6f7674429
Fikri Rasyid <fikrirasyid@gmail.com>
2015-02-22 08:03:05 +0700

    Removing unused post format conditionals

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -145,22 +145,6 @@ function the_archive_title( $before = '', $after = '' ) {
 	} elseif ( is_tax( 'post_format' ) ) {
 		if ( is_tax( 'post_format', 'post-format-aside' ) ) {
 			$title = _x( 'Asides', 'post format archive title', 'manuscript' );
-		} elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) {
-			$title = _x( 'Galleries', 'post format archive title', 'manuscript' );
-		} elseif ( is_tax( 'post_format', 'post-format-image' ) ) {
-			$title = _x( 'Images', 'post format archive title', 'manuscript' );
-		} elseif ( is_tax( 'post_format', 'post-format-video' ) ) {
-			$title = _x( 'Videos', 'post format archive title', 'manuscript' );
-		} elseif ( is_tax( 'post_format', 'post-format-quote' ) ) {
-			$title = _x( 'Quotes', 'post format archive title', 'manuscript' );
-		} elseif ( is_tax( 'post_format', 'post-format-link' ) ) {
-			$title = _x( 'Links', 'post format archive title', 'manuscript' );
-		} elseif ( is_tax( 'post_format', 'post-format-status' ) ) {
-			$title = _x( 'Statuses', 'post format archive title', 'manuscript' );
-		} elseif ( is_tax( 'post_format', 'post-format-audio' ) ) {
-			$title = _x( 'Audio', 'post format archive title', 'manuscript' );
-		} elseif ( is_tax( 'post_format', 'post-format-chat' ) ) {
-			$title = _x( 'Chats', 'post format archive title', 'manuscript' );
 		}
 	} elseif ( is_post_type_archive() ) {
 		$title = sprintf( __( 'Archives: %s', 'manuscript' ), post_type_archive_title( '', false ) );
