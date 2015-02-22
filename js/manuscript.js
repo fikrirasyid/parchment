6dd02de14aa13f9e028f68d5acfa25b7b2d6fccd
Fikri Rasyid <fikrirasyid@gmail.com>
2015-02-22 07:09:22 +0700

    Merging skip link focus fix

--- js/manuscript.js
+++ js/manuscript.js
@@ -66,3 +66,26 @@ function is_touch_device() {
 	return 'ontouchstart' in window // works on most browsers 
 		|| 'onmsgesturechange' in window; // works on ie10
 };
+
+/**
+* Skip link focus fix
+*/
+( function() {
+	var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
+	    is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
+	    is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
+
+	if ( ( is_webkit || is_opera || is_ie ) && document.getElementById && window.addEventListener ) {
+		window.addEventListener( 'hashchange', function() {
+			var element = document.getElementById( location.hash.substring( 1 ) );
+
+			if ( element ) {
+				if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) {
+					element.tabIndex = -1;
+				}
+
+				element.focus();
+			}
+		}, false );
+	}
+})();
